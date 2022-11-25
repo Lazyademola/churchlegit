ebd5b88dad95eba010e020ea7c8ff8c954b5468c
lazyademola <lazyademola@gmail.com>
2022-11-25 15:13:54 +0100

    newnewn

--- src/components/Give.jsx
+++ src/components/Give.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Give = () => {
   return (
-    <div name="give" className=' w-full text-white my-24'>
+    <div name="give" className=' w-full  my-24'>
         <div className=' w-full h-[800px] bg-teal-500 absolute mix-blend-overlay'></div>
         <div className='max-w-[1000px] mx-auto py-12'>
             <div className='text-center py-8 text-slate-300'>
                 <h1 className=' text-3xl uppercase' > Give Online</h1>
-                <h2 className=' text-5xl font-bold text-white py-8'>God loves a cheerful giver</h2>
+                <h2 className=' text-5xl font-bold text-black py-8'>God loves a cheerful giver</h2>
                 <p className=' text-2xl'>
                 Thank you for choosing to honour the LORD with your possessions, and with the first fruits of all your increase; so your barns will be filled with plenty,
                  and your vast will overflow with new wine in Jesus' name, Amen.
@@ -17,7 +17,7 @@ const Give = () => {
                 <p className=' text-xl font-semibold'>You can support the works of God in The NewBreed Faith Ministries by sending your seed to 
                 </p>
         
-                <h1 className='text-2xl text-red-400 font-bold'>"NEWBREED FAITH INT'L MINISTRIES " , Account Number:0123456789</h1>
+                <h1 className='text-2xl text-red-400 font-bold'>"NEWBREED FAITH INT'L MINISTRIES " , Account Number: 0123456789</h1>
             </div>
         </div>
         
